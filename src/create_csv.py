696226a8e1d5ed2f5a29222041333989aa28d21c
Sreya Francis <sreyafrancis.mec@gmail.com>
2019-04-21 00:01:00 -0400

    Scritpt to create Offchain Database

---
+++ src/create_csv.py
@@ -0,0 +1,35 @@
+import glob
+import csv
+
+path = '/home/user/Documents/src_1/clients/'
+n_devices = 5
+Model_version = 0
+l = len(path)
+i = 1
+
+Device_gradient_path = glob.glob(path+"*.block")
+print (Device_gradient_path)
+
+client_details = dict()
+
+for gradient in Device_gradient_path:
+    client_details[gradient] = gradient[ l+7 : l+9]
+
+csvData = [['Device_id', 'Device_delta_path', 'Model_version']]
+csvData = [[] for _ in range(n_devices+1)]
+csvData[0] = ['Device_id', 'Device_delta_path', 'Model_version']
+
+for k,v in client_details.items():
+    csvData[i].append(v)
+    csvData[i].append(k)
+    csvData[i].append(Model_version)
+    i += 1
+
+#print (csvData)
+
+with open('DeltaOffChainDatabase.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(csvData)
+
+csvFile.close()
+    
