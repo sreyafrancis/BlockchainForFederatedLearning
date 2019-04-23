af07ade367ae8d64583e14ace4cfdccaedfb2b3a
Sreya Francis <sreyafrancis.mec@gmail.com>
2019-04-23 14:19:00 -0400

    Update Run_BlockFL.sh

--- src/Run_BlockFL.sh
+++ src/Run_BlockFL.sh
@@ -2,16 +2,23 @@
 
 clear
 
-echo "Start miner with genesis block"
+echo "Creating datasets for n clients:"
+
+gnome-terminal -e "python3 data/federated_data_extractor.py"
+
+sleep 3
+
+echo "Start federated learning on n clients:"
 gnome-terminal -e "python3 miner.py -g 1 -l 2"
 
 sleep 3
 
-for i in `seq 0 9`;
+for i in `seq 0 1`;
         do
                 echo "Start client $i"
-                gnome-terminal -e "python3 client.py -d \"data/federated_data_$i.d\" -e 2"
+                gnome-terminal -e "python3 client.py -d \"data/federated_data_$i.d\" -e 1"
         done
 
+sleep 3
 
-
+gnome-terminal -e "python3 create_csv.py"
